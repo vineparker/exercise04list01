12f4e459e5017c2f93ee94a44498c94f71668a67
vineparker <viniciusalves.5262@aluno.una.br>
2023-04-18 01:50:37 -0300

    Update 0.2

--- src/App.java
+++ src/App.java
@@ -22,21 +22,21 @@ class Main {
     numero2 = input.nextInt();
 
     // terceira mensagem
-    String textoTres = "Qual sua nota do terceiro bimestre?:: ";
+    String textoTres = "Qual sua nota do terceiro bimestre?: ";
     System.out.print(textoTres);
     numero3 = input.nextInt();
 
     // quarta mensagem
-    String textoQuatro = "Qual sua nota do quarto bimestre?:: ";
+    String textoQuatro = "Qual sua nota do quarto bimestre?: ";
     System.out.print(textoQuatro);
     numero4 = input.nextInt();
     
-    // calcular a soma entre os dois numeros inputados
+    // calcular a media entre as notas dos 4 bimestres
     
     resultado = (numero1 + numero2 + numero3 + numero4) / 4;
 
     // mostrar o resultado da soma
-    System.out.println("Os dois numeros somados e igual a: " + resultado);
+    System.out.println("O a media da nota dos 4 bimetres e igual a: " + resultado);
 
     input.close();
   }
