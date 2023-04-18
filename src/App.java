6799c5df5901ab08ca7a411e96fdc7df14538170
vineparker <viniciusalves.5262@aluno.una.br>
2023-04-18 01:47:52 -0300

    Update

---
+++ src/App.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class Main {
+  // Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+  public static void main(String[] args) {
+
+    // declaração das variáveis 
+    Scanner input = new Scanner(System.in);int resultado; 
+    int numero1;
+    int numero2;
+    int numero3;
+    int numero4;
+
+    // primeira mensagem
+    String textoUm = "Qual sua nota do primeiro bimestre?: ";
+    System.out.print(textoUm);
+    numero1 = input.nextInt();
+
+    // segunda mensagem
+    String textoDois = "Qual sua nota do segundo bimestre?: ";
+    System.out.print(textoDois);
+    numero2 = input.nextInt();
+
+    // terceira mensagem
+    String textoTres = "Qual sua nota do terceiro bimestre?:: ";
+    System.out.print(textoTres);
+    numero3 = input.nextInt();
+
+    // quarta mensagem
+    String textoQuatro = "Qual sua nota do quarto bimestre?:: ";
+    System.out.print(textoQuatro);
+    numero4 = input.nextInt();
+    
+    // calcular a soma entre os dois numeros inputados
+    
+    resultado = (numero1 + numero2 + numero3 + numero4) / 4;
+
+    // mostrar o resultado da soma
+    System.out.println("Os dois numeros somados e igual a: " + resultado);
+
+    input.close();
+  }
+}
+
+
+
+
+
